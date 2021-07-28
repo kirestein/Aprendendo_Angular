1d00582898229118c64f9d380dd21448218db374
kirestein <erik.proenca2011@gmail.comn>
2021-07-28 20:45:48 -0300

    Dando os primeiros passos na liguagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-alpha';
+
+  // AQUI SERÃO IMPLEMENTADOS OS NOSSOS CÓDIGOS
+
+  /*AQUI VAMOS IMPLEMENTAR NOSSAS VARIAVEIS PARA ESTABELECER O INTERPOLATION DATA BINDING*/
+
+    public x: number = 10
+    public umArray: Array<number> = [10, 20, 50]
+    public dataHoje: Date = new Date()
+    public statusBooleano:boolean = true
+
+    /* vamos implementar um função */
+    public exibirTexto(): string{
+      return 'String exibida a partir do data binding interpolation!'
+    }
 }
+
+/*AQUI SERÁ O NOSSO REPOSITÓRIO DE CÓDIGOS QUE FUNCIONARAM*/
+
