581dde1ee81b3222a9287bca29450c8c2715e0f9
kirestein <erik.proenca2011@gmail.com>
2021-08-03 19:57:32 -0300

    Importando os novos componentes no app-routin.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
 
 const routes: Routes = [];
 
