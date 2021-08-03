5449158e1efea863336c65fa61f53d3f74cc339c
kirestein <erik.proenca2011@gmail.com>
2021-08-03 20:01:26 -0300

    Criando as rotas dos importes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+// Fazer o import dos componentes que serão roteados
 import { RouterModule, Routes } from '@angular/router';
 import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'faleconosco', component:FaleConoscoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
